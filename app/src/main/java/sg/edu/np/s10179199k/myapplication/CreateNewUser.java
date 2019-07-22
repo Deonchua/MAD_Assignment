57421e2b77828330970aba8181ff0365f40b141d
Xu Neng Deon Chua <s10179199@connect.np.edu.sg>
2019-07-22 22:36:36 +0800

    Design + a bit of basic button stuff

---
+++ app/src/main/java/sg/edu/np/s10179199k/myapplication/CreateNewUser.java
@@ -0,0 +1,21 @@
+package sg.edu.np.s10179199k.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateNewUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_user);
+    }
+
+    public void onCancel(View v)
+    {
+        Intent in = new Intent(CreateNewUser.this, MainActivity.class);
+        startActivity(in);
+    }
+}
--- app/src/main/java/sg/edu/np/s10179199k/myapplication/MainActivity.java
+++ app/src/main/java/sg/edu/np/s10179199k/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package sg.edu.np.s10179199k.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvNewUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNewUser = findViewById(R.id.textView4);
+
+        tvNewUser.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(MainActivity.this,CreateNewUser.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+
     }
+
 }
